02d5c8490e4df6b50adc3923602b40722b6bf81e
Bry Cox <carl.cc@gmail.com>
2019-01-29 20:45:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def line(katz_deli)
+  arr = []
+  katz_deli.each_with_index {|x, index| arr << "#{index+1}. #{x}"}
+  f = arr.join(" ")
+if katz_deli.count == 0
+  puts "The line is currently empty."
+else
+  puts "The line is currently: #{f}"
+end
+end
+
+def take_a_number(katz_deli, name)
+  name = []
+  name << katz_deli[0]
+  name
+end
